06a744d28a7bc5f610de0517ab7c620328be21af
sujaykumarmag <75253527+sujaykumarmag@users.noreply.github.com>
2021-11-17 11:34:27 +0530

    Add files via upload

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function SignIn(){
+    return(
+        <div>
+            <h1>Hello</h1>
+            <button class="login">
+                <img style={{width:"50px",height:"50px",paddingTop:"10px"}}src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google Logo"/>Log in With Google</button>
+        </div>
+    );
+}
+
+export default SignIn;
